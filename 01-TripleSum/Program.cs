a192916bad4310c4b9a7f50fefac8f66304c2197
aaroncustodio <custodio.aaron.1197@gmail.com>
2019-05-31 10:59:08 +0800

    added exer 02

--- 01-TripleSum/Program.cs
+++ 01-TripleSum/Program.cs
@@ -6,7 +6,22 @@ namespace _01_TripleSum
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(TripleSumIfEqual(1, 2));
+            Console.WriteLine(TripleSumIfEqual(3, 2));
+            Console.WriteLine(TripleSumIfEqual(2, 2));
+            Console.ReadKey();
+        }
+
+        static int TripleSumIfEqual(int a, int b)
+        {
+            if (a == b)
+            {
+                return 3 * (a + b);
+            }
+            else
+            {
+                return a + b;
+            }
         }
     }
 }
---
+++ AbsDiff/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AbsDiff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gogo(45));
+            Console.WriteLine(Gogo(30));
+            Console.WriteLine(Gogo(51));
+            Console.ReadKey();
+
+        }
+
+        static int Gogo(int n)
+        {
+            return (n > 51) ? (n - 51) * 3 : Math.Abs(n - 51);
+        }
+    }
+}
