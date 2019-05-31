2e7434bdf15978f1cb145ef50c58d81b7d08a44d
aaroncustodio <custodio.aaron.1197@gmail.com>
2019-05-31 11:10:12 +0800

    exer 03 done

---
+++ 03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Haha(30,10));
+            Console.WriteLine(Haha(20,10));
+            Console.WriteLine(Haha(15,15));
+            Console.WriteLine(Haha(10,34));
+            Console.ReadKey();
+        }
+
+        static Boolean Haha(int a, int b)
+        {
+            if (a==30 || b == 30 || a+b==30)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
